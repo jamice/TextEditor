709f6c94310fcb33bda3f4d3e84377a48a527d1c
Robert Deignan <robert.deignan@outlook.com>
2015-12-06 15:57:25 +0000

    update readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# TextEditor
+
+### About
+Simple text editor implemented in JavaFX. Allows loading and saving of files.
+
+### Bugs
+- None known.
+
+### Todo
+- Add more test.
---
+++ test/textEditor/util/FileToolsTest.java
@@ -0,0 +1,17 @@
+package textEditor.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Robert Deignan on 04/12/15.
+ * robert.deignan@outlook.com
+ */
+public class FileToolsTest {
+
+    @Test
+    public void testFileToString() throws Exception {
+
+    }
+}
